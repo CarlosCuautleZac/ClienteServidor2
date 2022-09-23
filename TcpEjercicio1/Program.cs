1441ffcd2ccd97106db3475e6015895e7bb7a981
AlumnoCCA1 <AlumnoCCA1@CCA5-07>
2022-09-23 14:55:11 -0500

    Agregar archivos de proyecto.

---
+++ TcpEjercicio1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPEndPoint ipe = new(IPAddress.Any, 2019);
+TcpListener listener = new(ipe);
+listener.Start();
+
+List<TcpClient> clients = new List<TcpClient>();
+
+while (true)
+{
+    TcpClient clienteConectado = listener.AcceptTcpClient();
+    Console.WriteLine("Se ha conectado un cliente: " + clienteConectado.Client.RemoteEndPoint.ToString());
+
+    clients.Add(clienteConectado);
+
+    //Iniciamos un hilo para que reciba mensajes del cliente nuevo
+    Thread hilo = new Thread(new ParameterizedThreadStart(RecibirDatos));
+    hilo.Start(clienteConectado);
+}
+
+void RecibirDatos(object? tcpclient)
+{
+    if (tcpclient != null)
+    {
+        TcpClient cliente = (TcpClient)tcpclient;
+
+        NetworkStream stream = cliente.GetStream();
+
+
+        while (cliente.Connected)
+        {
+            if (cliente.Available > 0)
+            {
+                byte[] buffer = new byte[cliente.Available];
+                stream.Read(buffer, 0, buffer.Length);
+
+                string mensaje = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine(mensaje);
+
+
+                //RETRANSMITIR (RELAY)
+                foreach (var c in clients)
+                {
+                    if (c != cliente)
+                    {
+                        EnviarMensjae(c, buffer);
+                    }
+                }
+            }
+
+            Thread.Sleep(1000);
+        }
+
+    }
+
+}
+
+void EnviarMensjae(TcpClient client, byte[] buffer)
+{
+    if (client.Connected)
+    {
+        var stream = client.GetStream();
+        stream.Write(buffer, 0, buffer.Length);
+    }
+}
---
+++ TcpEjercicio1Cliente/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Escribe la ip del servidor");
+var ip = Console.ReadLine();
+
+Console.WriteLine("Escriba el nombre de usuario");
+var username = Console.ReadLine();
+
+IPEndPoint ipe = new(IPAddress.Parse(ip), 2019);
+TcpClient tcpClient = new();
+
+//Conectarme
+while (true)
+{
+    tcpClient.Connect(ipe);
+    Thread.Sleep(2000);
+    if (tcpClient.Connected)
+    {
+        Thread t = new(new ThreadStart(Recibir));
+        t.Start();
+        break;
+
+    }
+}
+
+
+void Recibir()
+{
+    var stream = tcpClient.GetStream();
+    while (tcpClient.Connected)
+    {
+        if (tcpClient.Available > 0)
+        {
+            byte[] buffer = new byte[tcpClient.Available];
+            stream.Read(buffer, 0, buffer.Length);
+            string mensaje = Encoding.UTF8.GetString(buffer);
+            Console.WriteLine(mensaje);
+            
+        }
+        Thread.Sleep(1000);
+    }
+}
+
+//Enviar mensajes
+
+string? mensaje = "";
+var stream = tcpClient.GetStream();
+do
+{
+    mensaje = Console.ReadLine();
+    byte[] bytes = Encoding.UTF8.GetBytes($"{username}: {mensaje}");
+    stream.Write(bytes, 0, bytes.Length);
+
+
+} while (!string.IsNullOrWhiteSpace(mensaje));
+
+stream.Close();
+tcpClient.Close();
+
+Console.WriteLine("El servidor ha cerrado la conexion");
