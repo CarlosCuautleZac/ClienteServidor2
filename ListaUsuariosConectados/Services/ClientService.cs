d9716a117561c28133c7fb5b6591cbb8ac45bdfd
AlumnoCCA1 <AlumnoCCA1@CCA5-07>
2022-10-05 15:00:29 -0500

    user service

---
+++ ListaUsuariosConectados/Services/ClientService.cs
@@ -0,0 +1,75 @@
+﻿using ListaUsuariosConectados.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ListaUsuariosConectados.Services
+{
+    public class ClientService
+    {
+        TcpClient cliente = new();
+
+        public ClientService(string ip, Usuario usuario)
+        {
+            IPEndPoint ipe = new(IPAddress.Parse(ip), 55555);
+            cliente.Connect(ipe);
+            Thread.Sleep(1000);
+            Enviar(usuario);
+            Thread hilorecibir = new(new ThreadStart(Recibir));
+            hilorecibir.Start();    
+        }
+
+        public void Enviar(Usuario u)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(u);
+                byte[] bytes = Encoding.UTF8.GetBytes(json);
+                var stream = cliente.GetStream();
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception)
+            {
+                cliente.Close();    
+            }
+
+        }
+
+        public event Action<Usuario>? UsuarioRecibido;
+
+
+        void Recibir()//Recibir
+        {
+            var stream = cliente.GetStream();
+
+            while (cliente.Connected)
+            {
+                if (cliente.Available > 0)
+                {
+                    byte[] buffer = new byte[cliente.Available];
+                    stream.Read(buffer, 0, buffer.Length);
+                    string json = Encoding.UTF8.GetString(buffer);
+                    var usuario = JsonConvert.DeserializeObject<Usuario>(json);
+                    if(usuario != null)
+                    {
+                        UsuarioRecibido?.Invoke(usuario);
+                    }
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        public void Cerrar()
+        {
+            cliente.Close();
+        }
+    }
+}
---
+++ ListaUsuariosConectados/ViewModels/UsuariosViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ListaUsuariosConectados.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaUsuariosConectados.ViewModels
+{
+    public class UsuariosViewModel : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<Usuario> Usuarios { get; set; } = new ObservableCollection<Usuario>();
+
+        public bool SoyServidor { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Error { get; set; } = string.Empty;
+
+        public Usuario? usuario { get; set; }
+
+    }
+}
