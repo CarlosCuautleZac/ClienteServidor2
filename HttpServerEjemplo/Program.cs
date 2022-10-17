8b7c4e843dbe2d0da5fb80624912a2e1c1a285f4
AlumnoCCA1 <AlumnoCCA1@CCA5-07>
2022-10-17 14:51:29 -0500

    Creacion de server http

---
+++ HttpServerEjemplo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Text;
+
+HttpListener servidor = new();
+servidor.Prefixes.Add("http://*:10000/prueba/");
+servidor.Start();
+
+var context = servidor.GetContext();
+
+if (context.Request.Url.LocalPath == "/")
+{
+    string saludo = "hola";
+    byte[] buffer = Encoding.UTF8.GetBytes(saludo);
+    context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+    context.Response.StatusCode = 200;
+    context.Response.Close();
+}
